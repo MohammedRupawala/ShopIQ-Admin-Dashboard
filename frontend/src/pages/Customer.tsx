9b10a5a104bd5cf2fbc2047ee480ba17392565b9
Rupawala Mohammed Mustafa <mohammedrupawala8@gmail.com>
2024-06-19 12:59:29 +0530

    Costumer and Transaction Created

--- frontend/src/pages/Customer.tsx
+++ frontend/src/pages/Customer.tsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { ReactElement, useCallback, useState } from 'react'
 import AdminSide from '../components/adminSide'
+import { Column } from 'react-table'
+import TableHoc from '../components/TableHoc'
+import { FaTrash } from 'react-icons/fa'
+interface DataType{
+  avatar : ReactElement,
+  name :string,
+  gender : string,
+  email : string,
+  role: string,
+  action : ReactElement
+}
+const columns:Column<DataType>[]=[{
+  Header:"Avatar",
+  accessor:"avatar"  
+},
+{
+  Header:"Name",
+  accessor:"name"
+},
+{
+  Header:"Gender",
+  accessor:"gender"
+},
+{
+  Header:"Email",
+  accessor:"email"
+},
+{
+  Header:"Role",
+  accessor:"role"
+},
+{
+  Header:"Action",
+  accessor:"action"
+}]
+const img = "https://randomuser.me/api/portraits/women/54.jpg";
+const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
 
+const arr: DataType[] = [
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img}
+        alt="Shoes"
+      />
+    ),
+    name: "Emily Palmer",
+    email: "emily.palmer@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img2}
+        alt="Shoes"
+      />
+    ),
+    name: "May Scoot",
+    email: "aunt.may@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+];
 const Customer = () => {
+  const [data] = useState(arr);
+  const Table = useCallback(
+    TableHoc<DataType>(columns,data,"dashboardProductTable","Customer",true)
+    ,[])
   return (
     <div className='adminContainer'>
         <AdminSide/>
-        <main>ads</main>
+        <main>{Table()}</main>
     </div>
   )
 }
--- frontend/src/pages/Transaction.tsx
+++ frontend/src/pages/Transaction.tsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { ReactElement, useCallback, useState } from 'react'
 import AdminSide from '../components/adminSide'
-
+import TableHoc from '../components/TableHoc'
+import { Column } from 'react-table'
+import { Link } from 'react-router-dom'
+interface DataType{
+  user : string,
+  amount :number,
+  discount : number,
+  quantity : number,
+  status: ReactElement,
+  action : ReactElement
+}
+const columns:Column<DataType>[]=[{
+  Header:"User",
+  accessor:"user"  
+},
+{
+  Header:"Amount",
+  accessor:"amount"
+},
+{
+  Header:"Discount",
+  accessor:"discount"
+},
+{
+  Header:"Quantity",
+  accessor:"quantity"
+},
+{
+  Header:"Status",
+  accessor:"status"
+},
+{
+  Header:"Action",
+  accessor:"action"
+}]
+const arr: DataType[] = [
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="red">Processing</span>,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 6999,
+    discount: 400,
+    status: <span className="green">Shipped</span>,
+    quantity: 6,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 6999,
+    discount: 400,
+    status: <span className="purple">Delivered</span>,
+    quantity: 6,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+];
 const Transaction = () => {
+  const [data] =  useState(arr)
+  const Table = useCallback(
+  TableHoc<DataType>(columns,data,"dashboardProductTable","Transaction",true)
+  ,[])
   return (
     <div className='adminContainer'>
         <AdminSide/>
-        <main>ads</main>
+        <main>{Table()}</main>
     </div>
   )
 }
